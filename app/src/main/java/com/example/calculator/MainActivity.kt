b5c637b609920c0128fb56c3ad5cea0cf95033cf
kurosh97 <50096460+kurosh97@users.noreply.github.com>
2021-01-07 16:29:35 +0200

    added operation functionality using contains and startsWith

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -9,6 +9,10 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    var lastNumeric: Boolean = false
+    var lastDot: Boolean = false
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,13 +20,44 @@ class MainActivity : AppCompatActivity() {
 
 
     fun onDigit(view: View) {
-        val input = findViewById<TextView>(R.id.tvInput);
+        val input: TextView = findViewById<TextView>(R.id.tvInput);
+        lastNumeric = true
         input.append((view as Button).text)
+
+
     }
 
     fun onClear(view: View) {
-        val input = findViewById<TextView>(R.id.tvInput);
+        val input: TextView = findViewById<TextView>(R.id.tvInput);
+        lastNumeric = false
+        lastDot = false;
         input.text = ""
+    }
+
+    fun onDesimalPoint(view: View) {
+        val input: TextView = findViewById<TextView>(R.id.tvInput);
+        if (lastNumeric && !lastDot) {
+            input.append(".")
+            lastNumeric = false
+            lastDot = true;
+        }
+    }
+
+    fun onOperator(view: View) {
+        val input: TextView = findViewById<TextView>(R.id.tvInput);
+        if (lastNumeric && !isOperationAdded(input.text.toString())) {
+            input.append((view as Button).text)
+            lastNumeric = false
+            lastDot = false
+        }
+    }
+
+    private fun isOperationAdded(value: String): Boolean {
+        return if (value.startsWith("-")) {
+            false
+        } else {
+            value.contains("/") || value.contains("*") || value.contains("+") || value.contains("-")
+        }
     }
 
 
